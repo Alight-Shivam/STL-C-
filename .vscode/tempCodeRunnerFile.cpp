5c5a9a7b19481bf2a390d20242619ff3d62fb769
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2022-11-23 20:44:17 +0530

    STL

--- .vscode/tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-
-    // printVec(v);
---
+++ STL/Vectors.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printVec(vector<int> &v){
+    cout << "size: " << v.size() <<endl;
+    for(int i = 0; i<v.size(); ++i){
+        // v.size() -> O(1)
+         cout << v[i] << " ";
+    }
+v.push_back(2);
+cout << endl;
+}
+
+// int main(){
+//     vector<int> v;
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; ++i)
+//     {
+//         int x;
+//         cin>>x;
+//         printVec(v);
+//         v.push_back(x);  // Time Complexity is O(1)
+//     }
+//     printVec(v);
+// }
+
+
+// int main(){
+//     vector <int> v(10, 3);
+//     v.push_back(7);
+//     v.push_back(6);
+//     printVec(v);
+    
+//     v.pop_back();  //O(1)
+//     printVec(v);
+// }
+
+
+
+int main(){
+    vector<int> v;
+    v.push_back(7);
+    v.push_back(6);
+
+    vector<int> v2 = v;   //&v2 can also be used // Time Complexity is O(n)
+    v2.push_back(5);
+
+    printVec(v);
+    printVec(v);
+    printVec(v2);
+}
