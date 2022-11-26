3091adf7349cab787f23269e8bd5f0795e5677e3
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2022-11-26 17:27:44 +0530

    STL/C++

---
+++ STL/NestinginVectors.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// void printVec(vector<pair<int,int>> &v){
+//     cout << "size: " << v.size() <<endl;
+//     for(int i = 0; i < v.size(); ++i){
+//          cout << v[i].first << " " << v[i].second << endl;
+//     }
+
+// cout << endl;
+// }
+
+// Vectors of pairs
+
+// int main() {
+//      vector<pair<int,int>> v;
+//     // = {{1,2}, {2,3}, {4,5}}
+//     printVec(v);
+//     int n;
+//     cin>> n;
+//     for(int i = 0;i<n ; ++i){
+//         int x,y;
+//         cin>>x >>y;
+//         v.push_back({x,y});
+//     }
+//     printVec(v);
+// }
+
+// Vectors of arrays
+
+
+void printVec(vector<int> &v){
+    cout << "size: " << v.size() <<endl;
+    for(int i = 0; i < v.size(); ++i){
+         cout << v[i] << " " << endl;
+    }
+
+cout << endl;
+}
+
+// int main(){
+//     int N;
+//     cin >> N;
+//     vector<int> v[N];
+//     for(int i = 0; i < N; ++i){
+//         int n;
+//         cin >> n;
+//         for (int j = 0; j < n; ++j)
+//         {
+//             int x;
+//             cin >> x;
+//             v[i].push_back(x);
+//         }
+//         for(int i = 0; i < N; ++i){
+//             printVec(v[i]);
+//         }
+//     cout << v[0][1];
+//     }
+// }
+
+
+// Vector of Vectors 
+
+
+int main(){
+    int N;
+    cin >> N;
+    vector<vector<int> > v;
+    for(int i = 0; i < N; ++i){
+        int n;
+        cin >> n;
+        vector<int> temp;
+        for (int j = 0; j < n; ++j)
+        {
+            int x;
+            cin >> x;
+            temp.push_back(x);
+        }
+        v.push_back(temp);
+    }
+    v[0].push_back(10);
+    v.push_back(vector<int> ());
+    
+        for(int i = 0; i < v[N].size(); ++i){
+            printVec(v[i]);
+        }
+    cout << v[0][1];
+    }
--- STL/Vectors.cpp
+++ STL/Vectors.cpp
@@ -43,10 +43,26 @@ int main(){
     v.push_back(7);
     v.push_back(6);
 
-    vector<int> v2 = v;   //&v2 can also be used // Time Complexity is O(n)
+    vector<int> &v2 = v;   //&v2 can also be used // Time Complexity is O(n)
     v2.push_back(5);
 
     printVec(v);
     printVec(v);
     printVec(v2);
 }
+
+
+// taking input as string in vector
+// void printVec(vector<string> &v)
+
+// int main(){
+//     vector<string> v;
+//     int n;
+//     cin >> n;
+//     for(int i = 0; i < n; ++i){
+//         string s;
+//         cin >> s;
+//         v.push_back(s);
+//     }
+//     printVec(v); 
+// }
