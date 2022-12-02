7117515eef54926aa73c43cea4fe3fae5423f6f3
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2022-12-03 00:16:25 +0530

    STL/C++

--- STL/Sets.cpp
+++ STL/Sets.cpp
@@ -5,6 +5,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void print(set<string> &s){
+    for(string value : s){
+        cout << s << endl;
+    }
+    for(auto it = s.begin(); it != s.end(); ++it){
+        cout << (*it) << endl;
+    }
+}
+
 int main(){
 set<string> s;
 s.insert("abc");   // O(log(n))
