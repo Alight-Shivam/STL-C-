93318f792c3f74f8bc9c5d49763590b9de806b21
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2022-12-02 23:45:06 +0530

    STL/C++

--- STL/Sets.cpp
+++ STL/Sets.cpp
@@ -1,10 +1,15 @@
+// Normal Sets
+// Unordered Sets
+// Multisets
+
 #include<bits/stdc++.h>
 using namespace std;
 
-void print(set<string> &m){
+int main(){
+set<string> s;
+s.insert("abc");   // O(log(n))
+s.insert("zsdf");
+s.insert("bcd");
+auto it = s.find("abc");
 
-}
-
-int main{
-    
 }
