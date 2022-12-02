2e1ec5f5fb646219a1106feaf14fe1876d369efc
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2022-12-02 22:15:31 +0530

    STL/C++

---
+++ STL/Sets.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(set<string> &m){
+
+}
+
+int main{
+    
+}
--- STL/Umaps.cpp
+++ STL/Umaps.cpp
@@ -1,3 +1,29 @@
-// Inbuilt implementation
+// Inbuilt implementation Maps- RB Tree and UMaps- Hash Table
 // Time Complexity
 // valid keys datatype
+
+
+
+
+// int main(){
+//     unordered_map<int, string> m;
+//     m[1] = "abc";  // O(1) 
+//     m[5] = "cdc";
+//     m[3] = "acd";
+//     m.insert({4, "afg"});
+//     m[6];
+//     auto it = m.find(3);  // O(1)
+//     m.erase(3);  // O(1)
+//     m.clear();
+//     // if(it == m.end()){
+    //     cout << "no value";
+    // }
+    // else{
+    //     cout << (*it).first << " " << (*it).second;
+    // }
+ //   print(m);
+    // Unique Keys are stored and these keys are arranged in sorted order
+// }
+
+
+// pairs ke liye koi bhi hash function define nhi h isliye unordered maps ispe kaam nhi krte
