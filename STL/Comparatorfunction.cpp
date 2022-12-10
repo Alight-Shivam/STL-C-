81208374853bc212761bca58bd2f4362c515cc55
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2022-12-10 20:02:19 +0530

    STL/C++

--- STL/Comparatorfunction.cpp
+++ STL/Comparatorfunction.cpp
@@ -3,12 +3,51 @@ using namespace std;
 
 // Comparator Function
 
-bool should_i_swap(pair<int,int> a, pair<int,int> b){
-    if(a>b) return true;
-    else return false;
+// bool should_i_swap(pair<int,int> a, pair<int,int> b){
+//     if(a>b) return true;
+//     else return false;
+// }
+bool cmp(pair<int,int> a, pair<int,int> b){
+    if(a.first != b.first){
+        return a.first < b.first;
+        // if(a.first > b.first) return false;
+        // return true;
+    } 
+    return a.second > b.second;
 }
 
-  int main(){
+// bool cmp (int a, int b){
+//     return a>b;
+// }
+// Normal Behaviour of comparator function is Increasing order
+
+
+
+//   int main(){
+//     int n;
+//     cin >> n;
+//     vector <pair<int,int> > a(n);
+//     for (int i = 0; i < n; ++i)
+//     {
+//         cin >> a[i].first >> a[i].second ;
+//     }
+
+//     for(int i =0 ; i < n; ++i){
+//         for(int j = i + 1; j < n; ++j){
+//             if(should_i_swap(a[i],a[j])){
+//                 swap(a[i], a[j]);
+//             }
+//         }
+//     }
+//     for(int i = 0; i < n; ++i){
+//         cout << a[i].first << " " << a[i].second << endl;
+//     }
+//     cout << endl;
+//  }
+
+
+
+ int main(){
     int n;
     cin >> n;
     vector <pair<int,int> > a(n);
@@ -17,13 +56,7 @@ bool should_i_swap(pair<int,int> a, pair<int,int> b){
         cin >> a[i].first >> a[i].second ;
     }
 
-    for(int i =0 ; i < n; ++i){
-        for(int j = i + 1; j < n; ++j){
-            if(should_i_swap(a[i],a[j])){
-                swap(a[i], a[j]);
-            }
-        }
-    }
+    sort(a.begin(),a.end(), cmp); 
     for(int i = 0; i < n; ++i){
         cout << a[i].first << " " << a[i].second << endl;
     }
