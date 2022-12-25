f0a11373062444d6bd0a7833ab01b7579156024a
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2022-12-26 01:36:53 +0530

    STL/C++

--- STL/InbuiltAlgorithms.cpp
+++ STL/InbuiltAlgorithms.cpp
@@ -1,3 +1,5 @@
+// Time Complexity is O(n) in case of Vectors and arrays
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -12,4 +14,35 @@ int main(){
     int min = *min_element(v.begin(), v.end());
     // Ye Pointer return krta hai
     cout << min << endl ;
+    int max = *max_element(v.begin(), v.end());
+    // Ye Pointer return krta hai
+    cout << max << endl;
+    int sum = accumulate(v.begin(), v.end(), 0);
+    cout << sum << endl;
+    int ct = count(v.begin(), v.end(), 6);
+    cout << ct << endl;
+    int element = *find(v.begin(), v.end(), 2);
+    cout << element << endl;
+    reverse(v.begin(), v.end());
+    for (auto val : v){
+        cout << val << " ";
+    }
+    cout << endl;
+    string s = "abcdefghij";
+    reverse(s.begin(), s.end());
+    cout  << s << endl;
 }
+
+
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     int v[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> v[i];
+//     }
+//     int min = *min_element(v, v+n);
+//     cout << min << endl;
+// }
